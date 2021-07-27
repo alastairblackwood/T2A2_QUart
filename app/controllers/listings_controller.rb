b8baafec9cbbb99789a27f59113c228ac1137726
alastairblackwood <agrizzlybear@icloud.com>
2021-07-27 11:09:29 +1000

    new listing success

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,55 +1,54 @@
 class ListingsController < ApplicationController
-    before_action :set_listing, only: %i[ show edit update destory ]
-    before_action :setup_form, only: [new:, :edit]
+  before_action :set_listing, only: %i[ show edit update destory ]
+  before_action :setup_form, only: [:new, :edit]
 
-    # GET /listings or /listings.json
-    index
-    @listings = Listing.all
-    end
+  # GET /listings or /listings.json
+  def home
+  @listings = Listing.all
+  end
 
-    # GET /listings/1 or /listings/1.json
-    def show
-    end
+  # GET /listings/1 or /listings/1.json
+  def show
+  end
 
-    # GET /listings/new
-    def new
-        @listing = Listing.new
-    end
+  # GET /listings/new
+  def new
+    @listing = Listing.new
+  end
 
-    # GET /listings/1/edit
-    def edit
-    end
+  # GET /listings/1/edit
+  def edit
+  end
 
-    # POST /listings or /listings.json
-    def create
-        # @listing = Listing.new(listing_params)
-
-        @listing = current_user.listings.new(listing_params)
-    end
+  # POST /listings or /listings.json
+  def create
+    # @listing = Listing.new(listing_params)
+    @listing = current_user.listings.new(listing_params)
+  end
     
-    private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_listing
-      @listing = Listing.find(params[:id])
-    end
+  private
 
-    def set_user_listing
-      @listing = current_user.listings.find_by_id(params[:id])
-      if @listing == nil 
-        flash[:alert] = "You don't have permission to do that"
-        redirect_to listings_path
-      end
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
 
-    def setup_form
-      @categories = Category.all
-      @conditions = Listing.conditions.keys
-      @features = Feature.all
+  def set_user_listing
+    @listing = current_user.listings.find_by_id(params[:id])
+    if @listing == nil 
+      flash[:alert] = "You don't have permission to do that"
+      redirect_to listings_path
     end
+  end
 
-    # Only allow a list of trusted parameters through.
-    def listing_params
-      params.require(:listing).permit(:name, :picture, :description, :price, :category_id, :condition, feature_ids: [])
-    end
+  def setup_form
+    @categories = Category.all
+    # @conditions = Listing.conditions.keys
+    @features = Feature.all
+  end
 
+  # Only allow a list of trusted parameters through.
+  def listing_params
+    params.require(:listing).permit(:name, :picture, :description, :price, :category_id, :condition, feature_ids: [])
+  end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,4 @@
 class Listing < ApplicationRecord
   belongs_to :category
+  # enum condition: { seedling: 0, healthy: 1, old_plant: 2, unsure: 3 }
 end
