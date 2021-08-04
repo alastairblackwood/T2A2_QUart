b2fcb64cad8820e386e3d4e920e30eedd194214a
alastairblackwood <agrizzlybear@icloud.com>
2021-08-04 17:46:00 +1000

    config application change

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,8 @@ module QuArt
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
   end
 end
