fdd3fbf1ee16bc97004277a8ee01ecc0f260d37a
alastairblackwood <agrizzlybear@icloud.com>
2021-08-06 23:26:33 +1000

    restricted page add

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
+
+    before_action :authenticate_user!, only: [:restricted]
+
     def home
     end
 
     def about
+    end
+
+    def restricted
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   get 'payments/webhook'
   devise_for :users
   root to: 'pages#home'
+
   get '/about' => 'pages#about'
+  get '/restricted', to: 'pages#restricted', as: 'restricted'
   delete '/users/sign_out' => 'sessions#destroy'
   get '/users/sign_out' => 'sessions#destroy'
   resources :sessions
   resources :listings
+
   post '/payments/webhook', to: 'payments#webhook'
   get 'payments/success', to: 'payments#success'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
