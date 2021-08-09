3ad3543bb80133d3c20b38aba9384717af103907
alastairblackwood <agrizzlybear@icloud.com>
2021-08-09 15:04:37 +1000

    heroku delete fix

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,19 +3,30 @@ class PaymentsController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:webhook]
   
     def webhook
-      # pp params[:data][:object]
-  
+      p params
+      p "-----------------param------------------"
       payment_intent_id = params[:data][:object][:payment_intent]
       payment = Stripe::PaymentIntent.retrieve(payment_intent_id)
   
       listing_id = payment.metadata.listing_id
       buyer_id = payment.metadata.user_id
   
+      p "-----------------receipt----------------"
       receipt_url = payment.charges.data[0].receipt_url
+
+      p receipt_url
+
+      #Create a payment from database and declared attributes
+      Payment.create(user_id: buyer_id, listing_id: listing_id, payment_intent_id: payment_intent_id, receipt_url: receipt_url)
+
+      p params
+      p "---------------test-------------------"
+   
+      puts "test"
   
-      Order.create(user_id: buyer_id, listing_id: listing_id, payment_intent_id: payment_intent_id, receipt_url: receipt_url)
+      # Order.create(user_id: buyer_id, listing_id: listing_id, payment_intent_id: payment_intent_id, receipt_url: receipt_url)
   
-      # pp payment
+      # # pp payment
   
     end
   
